119dbb007e3bb7dd7b9b32bfbc2119bf84980424
Ramkishan01997 <ramkishan01997@gmail.com>
2022-08-19 07:27:47 +0530

    commit 3- addedd properties file and logging in the project

--- OpenCart-Automation/src/test/java/testBases/BaseClass.java
+++ OpenCart-Automation/src/test/java/testBases/BaseClass.java
@@ -1,28 +1,45 @@
 package testBases;
 
 import java.time.Duration;
+import java.util.ResourceBundle;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
 	public WebDriver driver;
+	public Logger logger;
+	public ResourceBundle rb;
 
 	@BeforeClass
-	public void setup()
-	{
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		
+	@Parameters("browser")
+	public void setup(String br) {
+		logger = LogManager.getLogger(this.getClass()); // for reading config file
+		rb = ResourceBundle.getBundle("config");
+		if (br.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (br.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			System.out.println(" please pass correct browser parameter");
+		}
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-		driver.get("https://demo.opencart.com/");
+//		driver.get("https://demo.opencart.com/");
+		driver.get(rb.getString("appUrl"));  // reading url from properties file
 		driver.manage().window().maximize();
 	}
 
--- OpenCart-Automation/src/test/java/testCases/TC_001AccountRegisterTest.java
+++ OpenCart-Automation/src/test/java/testCases/TC_001AccountRegisterTest.java
@@ -14,22 +14,23 @@ public class TC_001AccountRegisterTest extends BaseClass {
 
 			HomePage hp = new HomePage(driver);
 			hp.clickMyAccount();
-
+			logger.info("Clicked on My Account ");
 			hp.clickRegister();
-
+			logger.info("Clicked on Register ");
 			AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
 
 			regpage.setFirstName(randomeString().toUpperCase());
-
+			logger.info("Entered FirstName ");
 			regpage.setLastName(randomeString().toUpperCase());
-
+			logger.info("Entered Lastname ");
 			regpage.setEmail(randomeString() + "@gmail.com");// randomly generated the email
-
+			logger.info("Entered Email Id ");
 			regpage.setPassword("test@123");
-
+			logger.info("Entered Password ");
 			regpage.setPrivacyPolicy();
 
 			regpage.clickContinue();
+			logger.info("clicked on continue button ");
 
 		} catch (Exception e) {
 			Assert.fail();
